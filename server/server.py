2b7ed89f283fed53576f0d6e79fb8633d8ba175a
Tulika <tstdragoness@gmail.com>
2025-04-10 14:30:53 +0530

    Updating port listening

--- server/server.py
+++ server/server.py
@@ -178,4 +178,4 @@ def download_pdf():
         return jsonify({"success": False, "error": str(e)}), 500
     
 if __name__ == "__main__":
-    app.run(port=4000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 4000)), debug=True)
